2d8efd5e47636aeee88d64a92146254bca34244d
laxmantandon <laxmantandon@gmail.com>
2022-12-17 19:43:12 +0700

    Initial Commit

---
+++ geolife_agritech/geolife_agritech/doctype/crop/crop.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, Erevive Technologies and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Crop', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ geolife_agritech/geolife_agritech/doctype/crop_stage/test_crop_stage.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2022, Erevive Technologies and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestCropStage(FrappeTestCase):
+	pass
