7850d2f2b0e650076094ac6ab9c853db861657a0
kamesh0 <kamesh@erevive.in>
2024-03-02 11:39:37 +0000

    phase 2 testing api for live published

--- geolife_agritech/geolife_agritech/doctype/multiple_territory/multiple_territory.py
+++ geolife_agritech/geolife_agritech/doctype/multiple_territory/multiple_territory.py
@@ -1,8 +1,8 @@
-# Copyright (c) 2022, Erevive Technologies and contributors
+# Copyright (c) 2024, Erevive Technologies and contributors
 # For license information, please see license.txt
 
 # import frappe
 from frappe.model.document import Document
 
-class Territory(Document):
+class MultipleTerritory(Document):
 	pass
